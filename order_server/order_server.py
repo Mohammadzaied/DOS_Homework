9828d7101927a4015cdac54b8584d1a2d594980a
Mohammad Zaied <mohammad.zaied.8@gmail.com>
2023-11-11 12:22:32 +0200

    create order server Receives data from client server and Requests data from catalog server

---
+++ order_server/order_server.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import requests
+
+app = Flask(__name__)
+
+catalog_url = 'http://172.17.0.2:5100'
+
+@app.route('/purchase/<int:book_id>', methods=['PUT'])
+def purchase_book(book_id):
+
+    catalog_response = requests.put(f"{catalog_url}/catalog/{book_id}")
+    return catalog_response.json()
+
+if __name__ == '__main__':
+    app.run(debug=True,host='172.17.0.4',port=5200)
